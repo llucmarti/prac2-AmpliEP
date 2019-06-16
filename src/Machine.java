09793b4a5e27e8bce74797aec61a388750794051
llucmarti <47560772+llucmarti@users.noreply.github.com>
2019-06-16 08:24:13 +0200

    enunciat

---
+++ src/Machine.java
@@ -0,0 +1,6 @@
+public class Machine extends MachineComponent {
+
+    public boolean isBroken() {
+        return broken;
+    }
+}
---
+++ src/MachineComposite.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineComposite extends MachineComponent {
+    
+    private List<MachineComponent> components = new ArrayList<>();
+    public void addComponent(MachineComponent mc) {
+        components.add(mc);
+        }
+    public boolean isBroken() {
+        if (broken) { return true; }
+        for(MachineComponent mc: components) {
+            if (mc.isBroken()) { return true; }
+        }
+        return false;
+    }
+}
