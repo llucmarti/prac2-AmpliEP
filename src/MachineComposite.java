e32a3354c0c5e56213f35bd4cfea851626953027
llucmarti <47560772+llucmarti@users.noreply.github.com>
2019-06-16 08:38:58 +0200

    apartat c

--- src/MachineComposite.java
+++ src/MachineComposite.java
@@ -1,17 +1,64 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
-public class MachineComposite extends MachineComponent {
+public class MachineComposite extends MachineComponent implements Observer {
 
     private List<MachineComponent> components = new ArrayList<>();
-    public void addComponent(MachineComponent mc) {
+    private int brokenComponents = 0;
+    private boolean broken = false;
+    private boolean wasBroken;
+
+    public void addComponent(MachineComponent mc){
         components.add(mc);
+        mc.addObserver(this);
+        if(mc.isBroken()){
+            brokenComponents+=1;
+            if(!broken && brokenComponents == 1){
+                setChanged();
+                notifyObservers();
+            }
         }
-    public boolean isBroken() {
-        if (broken) { return true; }
-        for(MachineComponent mc: components) {
-            if (mc.isBroken()) { return true; }
+    }
+
+    public void setBroken(){
+        wasBroken = broken;
+        broken = true;
+        if(!wasBroken){
+            setChanged();
+            notifyObservers();
         }
-        return false;
+    }
+
+    public void repair() {
+        wasBroken = broken;
+        broken = false;
+        if(wasBroken){
+            setChanged();
+            notifyObservers();
+        }
+    }
+
+    public boolean isBroken(){
+        return broken || brokenComponents>0;
+    }
+
+    public void update(Observable observable, Object o) {
+        MachineComponent machineComponent = (MachineComponent) o;
+        if(machineComponent.isBroken()){
+            brokenComponents+=1;
+            boolean wasBroken = isBroken();
+            if(!wasBroken){
+                setChanged();
+                notifyObservers();
+            }
+        }else{
+            brokenComponents-=1;
+            if(!isBroken()){
+                setChanged();
+                notifyObservers();
+            }
+        }
     }
 }
